928cdd7c3e96da621ac34db1fc6db6c57be89ca1
Bryan Mansell <bryan@SpaceChimp.local>
2017-09-13 14:14:21 +0100

    Test 1.

---
+++ GitTest/GitTest/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Bryan Mansell on 13/09/2017.
+//  Copyright © 2017 Bryan Mansell. All rights reserved.
+//
+
+import UIKit
+
+
+//this is a test to check changes are committed.
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
